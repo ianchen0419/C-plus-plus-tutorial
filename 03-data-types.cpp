a5d478cb1ad1ac8c4963438871ebd32779fc3e2b
Yi An Chen <ianchen0419@gmail.com>
2022-11-02 20:08:41 +0800

    Create 03-data-types.cpp

---
+++ 03-data-types.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    // character: store one single character, need use single quote: `'`
+    char grade = 'A';
+    
+    // string: string of character
+    string phrase = "Giraffe Academy";
+    
+    // integar, can't store floating number
+    int age = 50;
+    
+    // float, store less floating number
+    
+    // double, store more floating number
+    double gpa = 2.3;
+    
+    // boolean
+    bool isMale = true;
+    
+    cout << grade  << endl;
+    
+    
+    return 0;
+}
