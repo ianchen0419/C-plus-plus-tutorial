4bc9a8905916c537598f2c43f12291d584b02cfa
Yi An Chen <ianchen0419@gmail.com>
2022-11-03 04:18:06 +0800

    Create 05-work-with-numbers.cpp

---
+++ 05-work-with-numbers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << 40 << endl;
+    cout << -40.123 << endl;
+    
+    cout << 1 + 2 << endl; // 12
+    
+    cout << 10 % 3 << endl; // 1
+    
+    cout << (4 + 5) * 10 << endl;
+    
+    // variable
+    int wnum = 5;
+    double dnum = 5.5;
+    
+    cout << dnum << endl; // 5.5
+    
+    wnum++; // wnum = wnum + 1;
+    cout << wnum << endl; // 6
+    
+    wnum += 80; // wnum = wnum + 80;
+    
+    // combine with whole number and decimal number
+    cout << 5.5 + 9 << endl; // 14.5
+    
+    cout << 10 / 3 << endl; // 3
+    
+    cout << 10.0 / 3.0 << endl; // 3.3333
+    
+    return 0;
+}
