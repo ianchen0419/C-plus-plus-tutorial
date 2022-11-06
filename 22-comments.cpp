0846702e0d35cc924dbe7f8446b10b90bbc1a4de
Yi An Chen <ianchen0419@gmail.com>
2022-11-06 21:17:02 +0800

    Create 22-comments.cpp

---
+++ 22-comments.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    // Comment
+    
+    /*
+      Long Comment
+     */
+    
+    /**
+     Long Comment
+     1. memo
+     2. memo
+     */
+    
+    
+    return 0;
+}
