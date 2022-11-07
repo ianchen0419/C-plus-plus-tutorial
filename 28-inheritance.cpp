0fa4db32f81241b62a903673dfa33824d06d8976
Yi An Chen <ianchen0419@gmail.com>
2022-11-07 22:02:18 +0800

    Create 28-inheritance.cpp

---
+++ 28-inheritance.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Chef {
+public:
+    void makeChiken() {
+        cout << "The chef makes chicken" << endl;
+    }
+    
+    void makeSalad() {
+        cout << "The chef makes salad" << endl;
+    }
+    
+    void makeSpecialDish() {
+        cout << "The chef makes BBQ ribs" << endl;
+    }
+};
+
+class ItalianChef : public Chef {
+public:
+    void makePasta() {
+        cout <<  "The chef makes pasta" << endl;
+    }
+    
+    void makeSpecialDish() {
+        cout << "The chef makes chicken parm" << endl;
+    }
+};
+
+int main() {
+    
+    Chef chef;
+    chef.makeChiken();
+    
+    ItalianChef italianChef;
+    italianChef.makeSalad();
+    
+    return 0;
+}
